5dc70045551aa6714cd74593675d0ac8ecf5861e
pran972e <pruthul_pradeep.andy@mailbox.tu-dresden.de>
2024-05-17 18:30:51 +0200

    alu_decoder added

--- source/rtl/main_decoder.v
+++ source/rtl/main_decoder.v
@@ -11,15 +11,21 @@ module main_decoder (
     wire branch;
 
     assign RegWrite = (Opcode == 7'b0000011 | Opcode == 7'b0110011) ? 1'b1 : 1'b0;
+
     assign ImmSrc = (Opcode == 7'b0100011 ) ? 2'b01 : 
                     (Opcode == 7'b1100011 ) ? 2'b10 : 2'b00;
+
     assign ALUSrc = (Opcode == 7'b0000011 | Opcode == 7'b0100011) ? 1'b1 : 1'b0;
+
     assign MemWrite = (Opcode == 7'b0100011) ? 1'b1 : 1'b0;
+
     assign ResultSrc = (Opcode == 7'b0000011) ? 1'b1 : 1'b0;
-    assign ResultSrc = (Opcode == 7'b0000011) ? 1'b1 : 1'b0;
+
     assign branch = (Opcode == 7'b1100011) ? 1'b1 : 1'b0;
+
     assign ALUOp = (Opcode == 7'b0110011 ) ? 2'b10 : 
                     (Opcode == 7'b1100011 ) ? 2'b01 : 2'b00;
+
     assign PCSrc = ZeroFlag & branch;
 
     

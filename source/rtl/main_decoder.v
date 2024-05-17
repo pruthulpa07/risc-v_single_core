a9fb2c7e89d6dee3b17ee7c411c336147ba59d69
pran972e <pruthul_pradeep.andy@mailbox.tu-dresden.de>
2024-05-17 13:29:52 +0200

    main_decoder added

---
+++ source/rtl/main_decoder.v
@@ -0,0 +1,26 @@
+module main_decoder (
+    Opcode, ZeroFlag, PCSrc, ResultSrc, MemWrite, ALUSrc, ImmSrc, RegWrite, ALUOp
+);
+
+    input [6:0] Opcode;
+    input ZeroFlag;
+
+    output PCSrc, ResultSrc, MemWrite, ALUSrc, RegWrite;
+    output [1:0] ImmSrc, ALUOp;
+
+    wire branch;
+
+    assign RegWrite = (Opcode == 7'b0000011 | Opcode == 7'b0110011) ? 1'b1 : 1'b0;
+    assign ImmSrc = (Opcode == 7'b0100011 ) ? 2'b01 : 
+                    (Opcode == 7'b1100011 ) ? 2'b10 : 2'b00;
+    assign ALUSrc = (Opcode == 7'b0000011 | Opcode == 7'b0100011) ? 1'b1 : 1'b0;
+    assign MemWrite = (Opcode == 7'b0100011) ? 1'b1 : 1'b0;
+    assign ResultSrc = (Opcode == 7'b0000011) ? 1'b1 : 1'b0;
+    assign ResultSrc = (Opcode == 7'b0000011) ? 1'b1 : 1'b0;
+    assign branch = (Opcode == 7'b1100011) ? 1'b1 : 1'b0;
+    assign ALUOp = (Opcode == 7'b0110011 ) ? 2'b10 : 
+                    (Opcode == 7'b1100011 ) ? 2'b01 : 2'b00;
+    assign PCSrc = ZeroFlag & branch;
+
+    
+endmodule
